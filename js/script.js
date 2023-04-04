d0f0fccd0fefed76810fb85143099022b4c0ff5a
Saifursaimon~ <saimon32kg@gmail.com>
2023-04-04 19:49:32 +0600

    updated

--- js/script.js
+++ js/script.js
@@ -1,6 +1,1 @@
-const buttons = document.querySelectorAll(".btn-select");
-for (const button of buttons) {
-  button.addEventListener("click", function () {
-    button.setAttribute("disabled", true);
-  });
-}
+
